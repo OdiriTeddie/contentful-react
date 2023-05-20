7a26199cc55f9c8365a33a6eb4490c87426f8af9
Odiri Teddie <odiriteddie.adamu@gmail.com>
2023-05-20 14:52:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Project from "./Project";
+
+const App = () => {
+  return (
+    <main>
+      <Hero />
+      <Project />
+    </main>
+  );
+};
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,24 @@
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ut
+            odit cupiditate accusamus laboriosam minima enim nobis. Minima neque
+            et ex, recusandae architecto cupiditate inventore impedit quo sint
+            nulla. Sint.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
